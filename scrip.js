e41aefa22febf4dc79c10d0960cea0d4baf40fd2
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-03-10 16:45:17 +0530

    page4 most of done

--- scrip.js
+++ scrip.js
@@ -1,5 +1,4 @@
 // loder page
-
 function loder() {
     var tlode = gsap.timeline();
 
@@ -14,76 +13,106 @@ loder()
 
 
 // locomotive and scrolltrigger
-function locoscroll(){
+function locoscroll() {
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
+    // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
 
 }
 locoscroll()
 
 
 // page3 scroolltrigger
+function page3scroll() {
+    var tl3 = gsap.timeline();
 
+    tl3.from(".elem h1,.elem-right h5", {
+        y: -100,
+        duration: 1,
+        ease: "sine.out",
+        scrollTrigger: {
+            trigger: ".b-right .images",
+            scroller: ".main",
+            start: "top 30%",
+            scrub: true
+        }
 
-var tl3 = gsap.timeline();
-
-tl3.from(".elem h1,.elem-right h5",{
-    y:-100,
-    duration:1,
-    ease: "sine.out",
-    scrollTrigger:{
-        trigger:".b-right .images",
-        scroller:".main",
-        start:"top 30%",
-        scrub:true
-    }
-
-})
-
-
-//page3 images
-
-var elemcoin = document.querySelector(".elem-container")
-var fiximg = document.querySelector(".fix-image")
-elemcoin.addEventListener('mouseenter',()=>{
-    fiximg.style.display = "block"
-})
-
-elemcoin.addEventListener('mouseleave',()=>{
-    fiximg.style.display = "none"
-})
-
-var elems = document.querySelectorAll(".elem")
-elems.forEach((e)=>{
-    e.addEventListener('mouseenter',function(){
-        var img = e.getAttribute("data-img")
-        console.log(img)
-        fiximg.style.backgroundImage = `url(${img})`
     })
-}) 
+
+    //page3 images
+
+    var elemcoin = document.querySelector(".elem-container")
+    var fiximg = document.querySelector(".fix-image")
+    elemcoin.addEventListener('mouseenter', () => {
+        fiximg.style.display = "block"
+    })
+
+    elemcoin.addEventListener('mouseleave', () => {
+        fiximg.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach((e) => {
+        e.addEventListener('mouseenter', function () {
+            var img = e.getAttribute("data-img")
+            console.log(img)
+            fiximg.style.backgroundImage = `url(${img})`
+        })
+    })
+}
+page3scroll()
+
+
+// swipper
+function swap(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3.6,
+        spaceBetween: 20,
+        freeMode: true,
+      });
+}
+swap()
+
+
+// page4
+
+var work_img = document.querySelector(".work-right")
+var work_info = document.querySelector("workinfo .chnage")
+
+var work_name = document.querySelectorAll(".workname h1")
+var flag = 0
+work_name.forEach((ele)=>{
+    ele.addEventListener ('click' , function (){
+            var addimg = ele.getAttribute("data-images")
+        var addtext = ele.getAttribute("data-info")
+        console.log(addtext)
+        // ele.style.color="red"
+        work_img.style.backgroundImage= `url(${addimg})`;
+        work_info.innerHTML = dataset.addtext;
+    })
+})
